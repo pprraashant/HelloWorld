d234c2df749e2c1a9604274dbeb27546b864d6f0
pprraashant <prashant2serve4u@gmail.com>
2017-06-10 15:33:00 +0530

    Added Code to HelloWorld2.java

---
+++ HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+        static String Message="Hello World2 Java Again";
+        public static void main(String args[])
+        {
+                System.out.println(Message);
+        }
+}
