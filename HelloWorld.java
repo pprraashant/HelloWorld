92a7f1fc4499c6415f1177f56dfa789eb4d03e53
pprrashant <prashant2serve4u@gmail.com>
2017-06-10 14:46:06 +0530

    Println change added

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld
 	static String Message="Hello World Java Again";
 	public static void main(String args[])
 	{
-		System.out.print(Message);
+		System.out.println(Message);
 	}
 }
